6ae6fbe522083ec5d3cf76139e6e9ffd34352616
seshathri044 <seshathri044@users.noreply.github.com>
2025-08-10 12:29:59 +0000

    deploy: 5130cb6d33a19677ec3ab9f74e0a0751c89fca84

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "c2d5e234c7f648c140c516159f0d4f5d",
+const RESOURCES = {"flutter_bootstrap.js": "4afd0a8c85fd7a1fcf0e2c717f29b93f",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
